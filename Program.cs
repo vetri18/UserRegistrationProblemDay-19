06d9442d59d807a285ed34fdbac34b180a38ff8c
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-25 12:00:53 +0530

    enter a valid First Name uc 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace UserReg
+﻿namespace RegexDemo
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Program");
+            Console.WriteLine("Please enter the First Name ");
+            string firstName = Console.ReadLine();
+
+            UserRegi user = new UserRegi();
+            bool val = user.FirstName(firstName);
+
+            if (val)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Matched");
+            }
         }
     }
 }
