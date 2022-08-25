58209a8ac9bffc56bfe49e099ce4df30dd8a67cb
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-25 12:01:38 +0530

    enter  a valid Last Name uc 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace UserReg
+﻿namespace RegexDemo
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Program");
+            //Console.WriteLine("Please enter the First Name ");
+            //string firstname = Console.ReadLine();
+
+
+            UserRegi user = new UserRegi();
+            //bool val = user.FirstName(firstname);
+
+            //if (val)
+            //{
+            //    Console.WriteLine("Pattern Matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pattern Not Matched");
+            //}
+
+            Console.WriteLine("Please enter the Last Name ");
+            string lastname = Console.ReadLine();
+            bool val1 = user.LastName(lastname);
+            if (val1)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Matched");
+            }
         }
     }
 }
---
+++ UserRegi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserRegi
+    {
+        public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, USER_FIRST_NAME);
+        }
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, USER_LAST_NAME);
+        }
+    }
+}
