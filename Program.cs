ea41a2eb91cd106bbc96e6f6b3c0de89279c33ba
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-25 12:02:22 +0530

    enter  a valid email uc 3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace UserReg
+﻿namespace RegexDemo
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Program");
+            //Console.WriteLine("Please enter the First Name ");
+            //string firstname = Console.ReadLine();
+
+
+            UserRegi user = new UserRegi();
+       
+
+            Console.WriteLine("Please enter email address:  ");
+            string email = Console.ReadLine();
+
+            bool val = user.Email(email);
+            if (val)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Matched");
+            }
         }
     }
 }
---
+++ UserRegi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserRegi
+    {
+        public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{3,20}$";
+        public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{3,20}$";
+        public static string USER_EMAIL = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
+        public static string MOBILE_NUMBER = "^([+][0-9]{2}[ ]){0,1}[6-9]{1}[0-9]{9}$";
+        public static string PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$^&+=?!])[0-9a-zA-Z]{8,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, USER_FIRST_NAME);
+        }
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, USER_LAST_NAME);
+        }
+        public bool Email(string email)
+        {
+            return Regex.IsMatch(email, USER_EMAIL);
+        }
+        public bool MobileNumber(string mobileNumb)
+        {
+            return Regex.IsMatch(mobileNumb, MOBILE_NUMBER);
+        }
+        public bool Password(string password)
+        {
+            return Regex.IsMatch(password, PASSWORD);
+        }
+    }
+}
